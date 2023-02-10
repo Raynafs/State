7a6cc5a2916758ecb56d093806a78b70bf817915
Raynafs <raynafs90@gmail.com>
2023-02-10 20:05:06 +0300

    Worked on the UI part using Jetpack Compose

---
+++ app/src/main/java/com/example/tip/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.tip
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tip.ui.theme.TipTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TipTheme {
+                Surface()
+                {
+                    Tip()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Tip()
+{
+Column (modifier = Modifier.padding(32.dp),
+    verticalArrangement = Arrangement.spacedBy(8.dp)
+)
+{
+    Text(
+        text = stringResource(id = R.string.calculate_tip),
+        fontSize= 24.sp,
+        modifier = Modifier.align(Alignment.CenterHorizontally)
+    )
+}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TipTheme {
+        Tip()
+    }
+}
