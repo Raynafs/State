edc054d8bb323ab1dc5629284f2b261733e5ea0e
Raynafs <raynafs90@gmail.com>
2023-02-21 19:47:44 +0300

    Recomposition proof and customized text field

--- app/src/main/java/com/example/tip/MainActivity.kt
+++ app/src/main/java/com/example/tip/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -53,7 +55,12 @@ Column (modifier = Modifier
         fontSize= 24.sp,
         modifier = Modifier.align(Alignment.CenterHorizontally)
     )
-    TextField(value = input, onValueChange = {input=it} )
+    TextField(value = input,
+        onValueChange = {input=it},
+        label = {Text(text= "value")},
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
     Button(onClick = { /*TODO*/ }) {
         Text(text= "Calculate")
     }
